f5e3beaed2a6d7afc488ba32941d39de405ca790
lucas <lucasportellaagu@gmail.com>
2021-09-28 11:32:32 -0300

    env

--- migrations/20210927200633-create-books.js
+++ migrations/20210927200633-create-books.js
@@ -2,20 +2,32 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Books');
   }
 };
